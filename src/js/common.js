aa36722af9975fa9d21339bd26dcf22232e67117
grygorash <sbuynyj@gmail.com>
2017-02-07 21:18:15 +0200

    mountains

---
+++ src/js/common.js
@@ -0,0 +1,28 @@
+ $(document).ready(function(){
+// регулярка
+$('.date').mask('00/00/0000');
+$('.phone_us').mask('+38 (000) 000-00-00')
+
+// ФИКСИРОВАННАЯ ШАПКА
+$("#header").removeClass("default");
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 0) {
+        $("#header").addClass("default").fadeIn('fast');
+    } else {
+        $("#header").removeClass("default").fadeIn('fast');
+    };
+});
+
+
+
+// ПЛАВНЫЙ СКРОЛЛ ПО ЯКОРЯМ
+$('a[href^="#"]').click(function(){
+    var el = $(this).attr('href');
+    $('body').animate({
+        scrollTop: $(el).offset().top -30}, 1000);
+    return false;
+});
+});
+
+
+
